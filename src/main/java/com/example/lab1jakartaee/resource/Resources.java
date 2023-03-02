4a505de616e264e6661d3e7194fd1328a1d364ef
William Karlström <wkarlstrom@outlook.com>
2023-03-02 14:52:23 +0100

    Add Resources with create JsonB method

---
+++ src/main/java/com/example/lab1jakartaee/resource/Resources.java
@@ -0,0 +1,16 @@
+package com.example.lab1jakartaee.resource;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.json.Json;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.ws.rs.Produces;
+
+@ApplicationScoped
+public class Resources {
+
+    @Produces
+    public Jsonb createJsonB(){
+        return JsonbBuilder.create();
+    }
+}
